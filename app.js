b34304b5342914136a1b95226a4b0bd87b9353c1
shreya gupta <shreyagupta0408@gmail.com>
2024-12-04 23:41:28 +0530

    2nd

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const app=express();
+const dotenv=require('dotenv');
+dotenv.config();
+const cors=require('cors'); 
 
 
+app.use(cors());
 
 
 app.get('/',(req,res)=>{
-    res.send("Hello wolrd");
+    res.send("Hello world");
 })
 
 
